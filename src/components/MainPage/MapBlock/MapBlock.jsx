f11a5fc8b3c59ba7178fa5ff1cf9a5100a130b71
GoduhinRoman <superchewits2000@gmail.com>
2020-06-15 19:03:35 +0300

    added primitive layout

--- src/components/MainPage/MapBlock/MapBlock.jsx
+++ src/components/MainPage/MapBlock/MapBlock.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './MapBlock.module.css'
 const MapBlock=(props)=>{
     return(
-        <div className={s.blockWrapper}>mapblock</div>
+        <div className={s.blockWrapper}>Mapblock</div>
     )
 }
 export default MapBlock;
