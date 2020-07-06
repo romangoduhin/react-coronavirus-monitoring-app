63611da155df342a020558d24c25028707103270
GoduhinRoman <superchewits2000@gmail.com>
2020-07-06 19:53:31 +0300

    feat: add StatPage search logic

--- src/components/StatPage/Statistic/Statistic.jsx
+++ src/components/StatPage/Statistic/Statistic.jsx
@@ -6,31 +6,47 @@ import {
 } from "../../../redux/covid-reducer";
 import { useDispatch, useSelector } from "react-redux";
 import s from "./Statistic.module.css";
+
 function Statistic() {
-  const { summaryCovidStat, globalCovidStat, countryCovidStat } = useSelector(
+  const [value, setValue] = useState("");
+  const { summaryCovidStat, globalCovidStat } = useSelector(
     (state) => state.covid
   );
+  const [arr, setArr] = useState(globalCovidStat);
+  console.log(arr);
   const dispatch = useDispatch();
   useEffect(() => {
     const getStat = async () => {
       const { Countries, Global } = await CovidAPI.getGlobalStatistics();
       dispatch(setSummaryStatActionCreator(Global));
       dispatch(setGlobalStatActionCreator(Countries));
+      setArr(Countries);
     };
     getStat();
   }, []);
+
+  useEffect(() => {
+    let filteredArr = globalCovidStat.filter((item) => {
+      return item.Country.includes(value);
+    });
+    setArr(filteredArr);
+  }, [value]);
 
   if (summaryCovidStat.length === 0 && globalCovidStat.length === 0)
     return <div> loading</div>;
   return (
     <div className={s.blockWrapper}>
-      {globalCovidStat.map((stat) => {
-        return (
-          <div>
-            {stat.Country} : {stat.TotalConfirmed}
-          </div>
-        );
-      })}
+      <input
+        className={s.input}
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
+      <div>
+        {arr.map((item) => {
+          return <div>{item.Country}</div>;
+        })}
+      </div>
     </div>
   );
 }
