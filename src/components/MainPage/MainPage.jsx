021acb0bb84531efadd9bc8bdc3db067d80e3a3a
GoduhinRoman <superchewits2000@gmail.com>
2020-07-08 19:11:49 +0300

    feat: add SympthomsBlock

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import s from "./MainPage.module.css"
+import React from "react";
+import s from "./MainPage.module.css";
 import NewsBlock from "./NewsBlock/NewsBlock";
 import StatBlock from "./StatBlock/StatBlock";
 import MapBlock from "./MapBlock/MapBlock";
 import NavBar from "../NavBar/NavBar";
-
+import SympthomsBlock from "./SympthomsBlock/SympthomsBlock";
 
 const MainPage = (props) => {
-    return (
-        <div className={s.wrapper}>
-            <NavBar/>
-            <div className={s.wrapperContent}>
-                <MapBlock/>
-                <StatBlock/>
-                <NewsBlock/>
-            </div>
-        </div>
-    )
+  return (
+    <div className={s.wrapper}>
+      <NavBar />
+      <div className={s.wrapperContent}>
+        <MapBlock />
+        <StatBlock />
+        <NewsBlock />
+        <SympthomsBlock />
+      </div>
+    </div>
+  );
 };
 export default MainPage;
